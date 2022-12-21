043dc048d74d4973e338d3ed3deee744f0980435
Franco-Castillo <93136916+Franco-Castillo@users.noreply.github.com>
2022-12-21 10:43:26 -0300

    Buen Comienzo
    desde aqui se puede partir bien

---
+++ My project/Assets/Script/MenuInicial.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+
+
+    public void Cambiar(){
+        SceneManager.LoadScene("Historia");
+    }
+
+}
