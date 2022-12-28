276112523ebb4a9288e680b7b9f32fc23f004f1e
Franco-Castillo <93136916+Franco-Castillo@users.noreply.github.com>
2022-12-27 22:28:01 -0300

    Desplazamiento
    jugador

--- My project/Assets/Script/Desplazamiento.cs
+++ My project/Assets/Script/Desplazamiento.cs
@@ -2,71 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Desplazamiento : MonoBehaviour
+public class Desplazamiento : MonoBehaviour //En "PlayerMovimiento" debe ir el nombre del archivo
 {
 
-    CharacterController characterController;
+    //Con los 'public float' se crea la variable con la que pueden editar desde la interfaz
+    //Al ser "public" se muestra, si fuera "private" no se vería en la interfaz
+    public float Speed = 1.0f;
+    public float RotationSpeed = 1.0f;
+    public float JumpForce = 1.0f;
 
-    public float velocidad_caminar=6.0f;
-
-    public float velocidad_correr=10.0f;
-
-    public float velocidad_salto=8.0f;
-
-    public float gravedad =70.0f;
-
-    private Vector3 movimiento = Vector3.zero;
-
-    // Start is called before the first frame update
+    private Rigidbody Physics;
 
     void Start()
-
     {
+        Cursor.visible = false; //Esta linea es para que cuando haga play y haga clic en el juego el cursor se esconde
 
-        characterController = GetComponent<CharacterController>();
-
+        Physics = GetComponent<Rigidbody>();
     }
-
-
-
-    // Update is called once per frame
 
     void Update()
-
     {
+        //Movimiento
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
-        if(characterController.isGrounded){
+        transform.Translate(new Vector3(horizontal, 0.0f, vertical) * Time.deltaTime * Speed);
 
-            movimiento= new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        //Rotación
+        float rotationY = Input.GetAxis("Mouse X");
 
-            if(Input.GetKey(KeyCode.LeftShift)){
+        transform.Rotate(new Vector3(0, rotationY * Time.deltaTime * RotationSpeed, 0));
 
-                movimiento = transform.TransformDirection(movimiento)* velocidad_correr; 
-
-            }
-
-            else{
-
-                movimiento = transform.TransformDirection(movimiento)* velocidad_caminar;
-
-            }
-
-            if(Input.GetKey(KeyCode.Space)){
-
-                movimiento.y = velocidad_salto;
-
-            }
-
-            
-
+        //Salto
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Physics.AddForce(new Vector3(0, JumpForce, 0), ForceMode.Impulse);
         }
 
-        movimiento.y -= gravedad * Time.deltaTime;
-
-        characterController.Move(movimiento*Time.deltaTime);
-
     }
-
 }
 
 
